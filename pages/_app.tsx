48698967d4c61033501453bf8ead06a493e6bdc1
Kyna <kyna.osprey@outlook.com>
2023-03-21 23:04:01 -0700

    Bootstrap Shenanigans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+// This must be in this order so the globals are not 'overwritten' by the bootstrap stuff
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
